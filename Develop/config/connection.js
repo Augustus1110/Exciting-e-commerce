b2ab697e99582242979e2d8be4b46a9002a00dd8
Augustus1110 <donahi22@gmail.com>
2023-08-04 13:47:00 -0700

    Configured server and set up category model

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 
 const Sequelize = require('sequelize');
-
+// ternery operator
+// true ? run this : otherwise run this
+// jawsdb ? use jawsdb : use local
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
